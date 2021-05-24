6f938f05a6963943dd5d6ff3d14da749718cd6eb
Kingmintchoco <70124129+Kingmintchoco@users.noreply.github.com>
2021-05-24 18:35:21 +0900

    LEVEL 1(종료)
    LV 1
    끝

--- 문자열을 정수로 바꾸기.cpp
+++ 문자열을 정수로 바꾸기.cpp
@@ -1,9 +1,32 @@
 #include <string>
 #include <vector>
+#include <cmath>
+#include <iostream>
 
 using namespace std;
 
 int solution(string s) {
     int answer = 0;
+
+    vector <char> tmp;
+    for (auto it = s.begin(); it != s.end(); ++it) {
+        tmp.push_back(*it);
+    }
+
+    int positive = 1;
+    for (int i = 0; i < tmp.size(); ++i) {
+        if (tmp[i] == '-') {
+            positive = -1;
+        }
+        else if (tmp[i] == '+') {
+            positive = 1;
+        }
+        else {
+            answer += ((tmp[i] - '0') * pow(10, tmp.size() - i - 1));
+            // answer += (tmp[i] * pow(10, tmp.size() - i - 1));
+        }
+    }
+
+    answer *= positive;
     return answer;
 }
---
+++ 최대공약수와 최소공배수.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// 최대공약수
+int gcd(int a, int b) {
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+// 최소공배수
+int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+
+    answer.push_back(gcd(n, m));
+    answer.push_back(lcm(n, m));
+    return answer;
+}
