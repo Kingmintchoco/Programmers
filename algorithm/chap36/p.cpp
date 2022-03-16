0a99081726908cf67de20f5995d70ad186a3f31f
Kingmintchoco <jrkr7@naver.com>
2022-03-16 18:55:58 +0900

    2022 03 16 WED

---
+++ algorithm/chap36/p.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector <int> insertSort(vector <int> v){
+    int i, j, tmp;
+    for(i = 1; i < v.size(); ++i){
+        tmp = v[i];
+        
+        for(j = i - 1; j >= 0; --j){
+            if(v[j] > tmp){
+                v[j + 1] = v[j];
+            }else{
+                break;
+            }
+        }
+
+        v[j + 1] = tmp;
+    }
+    return v;
+}
+
+int main(){
+    freopen("p.txt", "rt", stdin);
+    int n;
+    cin >> n;
+
+    int num;
+    vector <int> v;
+    for(int i = 0; i < n; ++i){
+        cin >> num;
+        v.push_back(num);
+    }
+
+    v = insertSort(v);
+
+    for(int i = 0; i < v.size(); ++i){
+        cout << v[i] << " ";
+    }
+}
