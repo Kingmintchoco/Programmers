020aae4bb3d531064e42743b87e910c5bafb733a
Kingmintchoco <jrkr7@naver.com>
2022-03-30 16:53:07 +0900

    2022 03 30 WED

--- algorithm/chap43/p.cpp
+++ algorithm/chap43/p.cpp
@@ -4,17 +4,40 @@
 
 using namespace std;
 
-int binarySearch(vector <int> v, int start, int end, int key){
-    int mid = (start + end) / 2;
+int n;
+vector <int> v;
 
-    if(v[mid] == key) return mid+1;
-    else if(v[mid] > key) return binarySearch(v, mid+1, end, key);
-    else return binarySearch(v, start, mid-1, key);
+int count(int s){
+    int i, cnt = 1, sum = 0;
+    for(i = 0; i < n; ++i){
+        if(sum + v[i] > s){
+            cnt++;
+            sum = v[i];
+        }else{
+            sum += v[i];
+        }
+    }
 
-    return mid+1;
+    return cnt;
 }
 
 int main(){
-    int n, m;
+    freopen("p.txt", "rt", stdin);
+    
+    int m, i, start = 1, end = 0, mid, res;
     cin >> n >> m;
+    for(i = 1; i <= n; ++i){
+        v.push_back(i);
+        end += i;
+    }
+
+    while(start <= end){
+        mid = (start + end)/2;
+        if(count(mid) <= m){
+            res = mid;
+            end = mid - 1;
+        }else start = mid + 1;
+    }
+
+    cout << res << "\n";
 }
