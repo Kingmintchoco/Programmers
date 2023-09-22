22ed11b50d4f9977d29669211816492e8ac3c405
Kingmintchoco <jrkr7@naver.com>
2023-09-22 15:56:51 +0900

    removed cached

--- Level1/옹알이(2)/p.cpp
+++ Level1/옹알이(2)/p.cpp
@@ -8,17 +8,6 @@ const string words[4] = {"aya", "ye", "woo", "ma"};
 
 int solution(vector<string> babbling) {
     int answer = 0;
-
-    int pos = 0, ret = 0;
-    for(int i = 0; i < babbling.size(); ++i){
-        for(int j = 0; j < 4; ++j){
-            if(babbling[i].find(words[j]) != string::npos){
-                pos = babbling[i].find(words[j]);
-                babbling[i]
-            }
-        }
-    }
-
     return answer;
 }
 
